13f46a48411880cd7f2d9de1c9cc299104f00458
simonguo <simonguo.2009@gmail.com>
2017-04-11 22:55:07 +0800

    Update

--- src/overlay/Position.js
+++ src/overlay/Position.js
@@ -7,28 +7,28 @@ import overlayPositionUtils from './overlayPositionUtils';
 
 const propTypes = {
     /**
-   * Function mapping props to a DOM node the component is positioned next to
-   *
-   */
+     * Function mapping props to a DOM node the component is positioned next to
+     *
+     */
     target: React.PropTypes.func,
 
     /**
-   * "offsetParent" of the component
-   */
-    container: React
-        .PropTypes
-        .oneOfType([mountable, React.PropTypes.func]),
+     * "offsetParent" of the component
+     */
+    container: React.PropTypes.oneOfType([mountable, React.PropTypes.func]),
     /**
-   * Minimum spacing in pixels between container border and component border
-   */
+     * Minimum spacing in pixels between container border and component border
+     */
     containerPadding: React.PropTypes.number,
+
     /**
-   * How to position the component relative to the target
-   */
+     * How to position the component relative to the target
+     */
     placement: React.PropTypes.oneOf(['top', 'right', 'bottom', 'left']),
+
     /**
-   * Whether the position should be changed on each update
-   */
+     * Whether the position should be changed on each update
+     */
     shouldUpdatePosition: React.PropTypes.bool
 };
 
