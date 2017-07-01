e5e2f77de8337cb99d96ecde65dff25ee7d2c94d
simonguo <simonguo.2009@gmail.com>
2017-07-01 23:05:54 +0800

    Update docs

---
+++ docs/ModalDemo.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Modal } from '../src/Overlay';
+import { Fade, Collapse } from '../src/Animation';
+
+class ModalDemo extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleHide = this.handleHide.bind(this);
+    this.state = {
+      show: false
+    };
+  }
+
+  handleOpenModal() {
+    this.setState({
+      show: true
+    });
+  }
+  handleHide(e) {
+    if (e && e.target !== e.currentTarget) {
+      return;
+    }
+    this.setState({
+      show: false
+    });
+  }
+
+  render() {
+    return (
+
+      <div className="row">
+
+        <button onClick={this.handleOpenModal}>open modal</button>
+        <Modal
+          backdrop
+          transition={Fade}
+          backdropClassName={'modal-backdrop'}
+          containerClassName={'modal-open'}
+          onHide={this.handleHide}
+          show={this.state.show}>
+          <div
+            className="modal"
+            style={{
+              display: this.state.show ? 'block' : 'none'
+            }}
+            onClick={this.handleHide}
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">Header</div>
+                <div className="modal-body">Body</div>
+                <div className="modal-footer">Footer</div>
+              </div>
+            </div>
+          </div>
+
+        </Modal>
+      </div>
+
+    );
+  }
+}
+
+export default ModalDemo;
--- docs/index.js
+++ docs/index.js
@@ -1,46 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Modal } from '../src/Overlay';
-import { Fade } from '../src/Animation';
+import ModalDemo from './ModalDemo';
 
 import './less/index.less';
 
 class App extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.handleOpenModal = this.handleOpenModal.bind(this);
-    this.handleHide = this.handleHide.bind(this);
-    this.state = {
-      show: false
-    };
-  }
-
-  handleOpenModal() {
-    this.setState({
-      show: true
-    });
-  }
-  handleHide() {
-    this.setState({
-      show: false
-    });
-  }
 
   render() {
     return (
       <div className="doc-page">
         <div className="container">
           <h1>RSuite Utils</h1>
-          <button onClick={this.handleOpenModal}>open modal</button>
-          <Modal
-            backdrop
-            transition={Fade}
-            backdropClassName={'modal-backdrop'}
-            containerClassName={'modal-open'}
-            onHide={this.handleHide}
-            show={this.state.show}>
-            <div className="modal-dialog">ddd</div>
-          </Modal>
+          <ModalDemo />
+
         </div>
       </div>
     );
