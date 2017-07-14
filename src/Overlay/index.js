1f41f7d3f9b8374e775e5f9db46c572e4564eebb
simonguo <simonguo.2009@gmail.com>
2017-07-14 11:50:01 +0800

    rm overlay

--- src/Overlay/index.js
+++
@@ -1,5 +0,0 @@
-export Modal from './Modal';
-export Overlay from './Overlay';
-export Position from './Position';
-export Portal from './Portal';
-export RootCloseWrapper from './RootCloseWrapper';
