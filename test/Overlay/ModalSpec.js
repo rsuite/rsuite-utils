e412b4a26db512750d8ed9bf39b57d4e083f25b0
simonguo <simon.guo@hypers.com>
2017-10-28 11:49:51 +0800

    Update test case

--- test/Overlay/ModalSpec.js
+++ test/Overlay/ModalSpec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM, { render, findDOMNode } from 'react-dom';
-import jQuery from 'jquery';
-import ReactTestUtils from 'react/lib/ReactTestUtils';
+import ReactTestUtils from 'react-dom/test-utils';
 import { Modal } from '../../src/Overlay';
 const $ = componentOrNode => jQuery(findDOMNode(componentOrNode));
 
@@ -16,7 +15,6 @@ describe('<Modal>', () => {
   it('Should render the modal content', () => {
     let instance = render(<Modal show ><p>message</p></Modal>, mountPoint);
     assert.equal(instance.modal.querySelectorAll('p').length, 1);
-
   });
 
 });
