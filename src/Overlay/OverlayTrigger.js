194e5a90e211f8b37b0927122d225c8002bc6702
simonguo <simon.guo@hypers.com>
2018-01-27 14:39:51 +0800

    Added support open and defaultOpen

--- src/Overlay/OverlayTrigger.js
+++ src/Overlay/OverlayTrigger.js
@@ -10,10 +10,11 @@ import isNullOrUndefined from '../utils/isNullOrUndefined';
 import createChainedFunction from '../utils/createChainedFunction';
 import handleMouseOverOut from '../utils/handleMouseOverOut';
 import isOneOf from '../utils/isOneOf';
-import OverlayTriggerLegacy from './OverlayTriggerLegacy';
 import Portal from './Portal';
 
 import type { Placement, DefaultEventFunction, DefaultEvent, TriggerName } from '../utils/TypeDefinition';
+
+const unsupportedCreatePortal = !ReactDOM.createPortal;
 
 type Props = {
   target?: Function,
@@ -35,14 +36,16 @@ type Props = {
   delay?: number,
   delayShow?: number,
   delayHide?: number,
-  defaultOverlayShown?: boolean,
+  defaultOpen?: boolean,
+  open?: boolean,
   speaker: React.Element<any>,
   children: React.Node,
   onMouseOver?: DefaultEventFunction,
   onMouseOut?: DefaultEventFunction,
   onClick?: DefaultEventFunction,
   onBlur?: DefaultEventFunction,
-  onFocus?: DefaultEventFunction
+  onFocus?: DefaultEventFunction,
+  disabled?: boolean
 }
 
 type OverlayTriggerProps = {
@@ -62,9 +65,9 @@ type States = {
 class OverlayTrigger extends React.Component<Props, States> {
 
   static defaultProps = {
-    defaultOverlayShown: false,
     trigger: ['hover', 'focus'],
     delayHide: 200,
+    placement: 'bottomLeft',
     rootClose: true
   };
 
@@ -75,13 +78,31 @@ class OverlayTrigger extends React.Component<Props, States> {
     this.handleMouseOut = (e: DefaultEvent) => handleMouseOverOut(this.handleDelayedHide, e);
 
     this.state = {
-      isOverlayShown: props.defaultOverlayShown
+      isOverlayShown: props.defaultOpen
     };
+  }
+
+  componentDidMount() {
+    if (unsupportedCreatePortal) {
+      this.mountNode = document.createElement('div');
+      this.renderOverlay();
+    }
+  }
+
+  componentDidUpdate() {
+    if (unsupportedCreatePortal && this.mountNode) {
+      this.renderOverlay();
+    }
   }
 
   componentWillUnmount() {
     clearTimeout(this.hoverShowDelay);
     clearTimeout(this.hoverHideDelay);
+
+    if (unsupportedCreatePortal) {
+      ReactDOM.unmountComponentAtNode(this.mountNode);
+      this.mountNode = null;
+    }
   }
 
   getOverlayTarget = () => findDOMNode(this) // eslint-disable-line react/no-find-dom-node
@@ -89,11 +110,12 @@ class OverlayTrigger extends React.Component<Props, States> {
 
   getOverlay() {
 
-
+    const { open, speaker } = this.props;
+    const { isOverlayShown } = this.state;
     const overlayProps = {
       ..._.pick(this.props, Overlay.handledProps),
-      show: this.state.isOverlayShown,
-      onHide: this.handleHide,
+      show: _.isUndefined(open) ? isOverlayShown : open,
+      onHide: this.hide,
       target: this.getOverlayTarget
     };
 
@@ -107,7 +129,7 @@ class OverlayTrigger extends React.Component<Props, States> {
       <Overlay
         {...overlayProps}
       >
-        {React.cloneElement(this.props.speaker, speakerProps)}
+        {React.cloneElement(speaker, speakerProps)}
       </Overlay>
     );
   }
@@ -118,6 +140,7 @@ class OverlayTrigger extends React.Component<Props, States> {
   hoverShowDelay = null;
   hoverHideDelay = null;
   target = null;
+  mountNode = null;
 
 
   enterSpeaker = false;
@@ -131,27 +154,28 @@ class OverlayTrigger extends React.Component<Props, States> {
     const { trigger } = this.props;
     this.enterSpeaker = false;
     if (!isOneOf('click', trigger)) {
-      this.hide();
+      this.handleHide();
     }
   }
 
-  hide() {
-    if (!this.enterSpeaker && !this.enterTrigger) {
-      this.setState({ isOverlayShown: false });
-    }
+  hide = () => {
+    this.setState({ isOverlayShown: false });
+
   }
 
-  show() {
+  show = () => {
     this.setState({ isOverlayShown: true });
   }
 
   handleHide = () => {
-    this.hide();
+    if (!this.enterSpeaker && !this.enterTrigger) {
+      this.hide();
+    }
   }
 
   handleToggle = () => {
     if (this.state.isOverlayShown) {
-      this.hide();
+      this.handleHide();
     } else {
       this.show();
     }
@@ -204,7 +228,7 @@ class OverlayTrigger extends React.Component<Props, States> {
     const nextDelay = !isNullOrUndefined(delayHide) ? delayHide : delay;
 
     if (!nextDelay) {
-      this.hide();
+      this.handleHide();
       return;
     }
 
@@ -215,10 +239,15 @@ class OverlayTrigger extends React.Component<Props, States> {
       }
       clearTimeout(this.hoverHideDelay);
       this.hoverHideDelay = null;
-      this.hide();
+      this.handleHide();
     }, nextDelay);
   }
 
+  renderOverlay() {
+    if (this.speaker) {
+      ReactDOM.unstable_renderSubtreeIntoContainer(this, this.speaker, this.mountNode);
+    }
+  }
 
   render() {
     const {
@@ -229,7 +258,8 @@ class OverlayTrigger extends React.Component<Props, States> {
       onMouseOver,
       onMouseOut,
       onFocus,
-      onBlur
+      onBlur,
+      disabled
     } = this.props;
 
     const triggerComponent = React.Children.only(children);
@@ -243,43 +273,51 @@ class OverlayTrigger extends React.Component<Props, States> {
 
     props.onClick = createChainedFunction(triggerProps.onClick, onClick);
 
-    if (isOneOf('click', trigger)) {
-      props.onClick = createChainedFunction(this.handleToggle, props.onClick);
+    if (!disabled) {
+      if (isOneOf('click', trigger)) {
+        props.onClick = createChainedFunction(this.handleToggle, props.onClick);
+      }
+
+      if (isOneOf('hover', trigger)) {
+        props.onMouseOver = createChainedFunction(
+          this.handleMouseOver,
+          onMouseOver,
+          triggerProps.onMouseOver
+        );
+        props.onMouseOut = createChainedFunction(
+          this.handleMouseOut,
+          onMouseOut,
+          triggerProps.onMouseOut
+        );
+      }
+
+      if (isOneOf('focus', trigger)) {
+
+        props.onFocus = createChainedFunction(
+          this.handleDelayedShow,
+          onFocus,
+          triggerProps.onFocus
+        );
+
+        props.onBlur = createChainedFunction(
+          this.handleDelayedHide,
+          onBlur,
+          triggerProps.onBlur
+        );
+      }
     }
 
-    if (isOneOf('hover', trigger)) {
-      props.onMouseOver = createChainedFunction(
-        this.handleMouseOver,
-        onMouseOver,
-        triggerProps.onMouseOver
-      );
-      props.onMouseOut = createChainedFunction(
-        this.handleMouseOut,
-        onMouseOut,
-        triggerProps.onMouseOut
-      );
-    }
-
-    if (isOneOf('focus', trigger)) {
-
-      props.onFocus = createChainedFunction(
-        this.handleDelayedShow,
-        onFocus,
-        triggerProps.onFocus
-      );
-
-      props.onBlur = createChainedFunction(
-        this.handleDelayedHide,
-        onBlur,
-        triggerProps.onBlur
-      );
+    if (unsupportedCreatePortal) {
+      this.speaker = this.getOverlay();
+      return React.cloneElement(triggerComponent, props);
     }
 
     return [
       React.cloneElement(triggerComponent, props),
       <Portal key="portal">{this.getOverlay()}</Portal>
     ];
+
   }
 }
 
-export default ReactDOM.createPortal ? OverlayTrigger : OverlayTriggerLegacy;
+export default OverlayTrigger;
