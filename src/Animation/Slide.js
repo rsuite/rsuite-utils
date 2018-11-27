4dc9e1435e04ddebfa36bd843f83c4c2066b9fe7
simonguo <simon.guo@hypers.com>
2018-11-27 10:31:33 +0800

    Support placement on `<Slide>`

--- src/Animation/Slide.js
+++ src/Animation/Slide.js
@@ -1,28 +1,34 @@
 // @flow
 
 import * as React from 'react';
+import classNames from 'classnames';
 import Transition from './Transition';
 
 type Props = {
   timeout?: number,
-  in?: boolean
+  in?: boolean,
+  placement?: 'top' | 'right' | 'bottom' | 'left'
 };
 
 class Slide extends React.Component<Props> {
   static displayName = 'Slide';
   static defaultProps = {
+    placement: 'right',
     timeout: 300
   };
 
   render() {
-    const { timeout, ...props } = this.props;
+    const { timeout, placement, ...props } = this.props;
+    const enteringClassName = classNames('slide-in', placement, 'animated');
+    const exitingClassName = classNames('slide-out', placement, 'animated');
+
     return (
       <Transition
         {...props}
         animation
         timeout={timeout}
-        enteringClassName="slide-in animated"
-        exitingClassName="slide-out animated"
+        enteringClassName={enteringClassName}
+        exitingClassName={exitingClassName}
       />
     );
   }
